a2394e98540a440fd786b206893e29ad52512fa3
30p87 <50525850+30p87@users.noreply.github.com>
2022-04-10 09:59:55 +0200

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,18 @@
+If ((Test-Path "$Env:AppData\notflix.py") -eq $False) {
+  mkdir $Env:AppData\patchjar
+}
+
+cp main.py $Env:AppData\patchjar
+
+$is = $False
+foreach ($_path in ($Env:PATH).split(";")) {
+  If ("$_path" -match "patchjar") {
+    $is = $True
+  }
+}
+
+If ($is -eq $False) {
+  $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+  $newpath = "$oldpath;$Env:AppData\patchjar"
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+}
